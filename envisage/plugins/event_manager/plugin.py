0367502897eef78972586807ba6c36c258f3704d
Pankaj Pandey <pankaj@enthought.com>
2011-12-13 17:44:49 +0530

    EventManagerPlugin: Lazy import and do not set application attribute.
    Do the EventManager import lazily on service object creation.
    Also, the event manager instance is not set as an attribute of the
    application since it may create problems in debugging later on.

--- envisage/plugins/event_manager/plugin.py
+++ envisage/plugins/event_manager/plugin.py
@@ -5,21 +5,16 @@
 #
 # This file is open source software distributed according to the terms in LICENSE.txt
 #
-""" This module provides a plugin which adds an EventManager to application.
+""" This module provides a plugin which adds an EventManager instance as
+a service to the application.
 
-If the application does not already have an evt_mgr attribute which is an
-instance of EventManager, the plugin creates a new EventManager instance,
-creates a service to offer the event manager and sets the evt_mgr instance
-of the application to the created event manager.
 """
 
 # Enthought library imports.
 from envisage.api import Plugin, ServiceOffer
-from traits.api import List, Any
+from traits.api import List
 
-# Local imports.
-from encore.events.event_manager import EventManager
-
+EVENT_MANAGER_PROTOCOL = 'encore.events.abstract_event_manager.BaseEventManager'
 
 class EventManagerPlugin(Plugin):
     """ Plugin to add event manager to the application. """
@@ -29,23 +24,13 @@ class EventManagerPlugin(Plugin):
     SERVICE_OFFERS = 'envisage.service_offers'
     service_offers = List(contributes_to=SERVICE_OFFERS)
 
-    evt_mgr = Any
-
     def _service_offers_default(self):
-        evt_mgr = self.evt_mgr # Ensure evt_mgr is created
         evt_mgr_service_offer = ServiceOffer(
-            protocol   = EventManager,
-            factory    = lambda: evt_mgr,
+            protocol   = EVENT_MANAGER_PROTOCOL,
+            factory    = lambda: self._create_event_manager,
         )
         return [evt_mgr_service_offer]
 
-    def _evt_mgr_default(self):
-        """ If application already has an EventManager, then that is returned.
-        """
-        app = self.application
-        evt_mgr = getattr(app, 'evt_mgr', None)
-        if evt_mgr is None:
-            evt_mgr = EventManager()
-            app.evt_mgr = evt_mgr
-        return evt_mgr
-
+    def _create_event_manager(self):
+        from encore.events.event_manager import EventManager
+        return EventManager()
